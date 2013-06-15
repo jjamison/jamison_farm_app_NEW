0df0417f69822f7cc8215307592ca72eb42899e0
Jay Jamison <jeremiah.s.jamison@gmail.com>
2013-06-15 12:54:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JamisonFarmApp::Application.config.secret_token = 'f27e7d07511f6a46fe3d5926aa43f83fad80ac68fa19a46f3f333bf38d00838822e65c2c7a53439d7150a069a7be2d087582d322560f23b420881df14fed4464'
